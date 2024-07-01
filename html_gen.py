76671362c15bbb877f6066b4d986f819f296ced8
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2024-07-01 17:26:15 +0300

    Add Table Name as input

--- html_gen.py
+++ html_gen.py
@@ -5,6 +5,9 @@ import pandas as pd
 st.title("HTML Table Generator")
 st.write("Upload a CSV file or input data manually to generate an HTML table.")
 
+# Table name input
+table_name = st.text_input("Enter the Table Name:", "Generated Table")
+
 # Display example CSV input
 st.subheader("Example CSV Input")
 example_data = {
@@ -22,27 +25,27 @@ uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
 manual_input = st.text_area("Or manually input data (comma-separated values, new line for each row)", "")
 
 # Function to generate HTML from DataFrame
-def generate_html(df):
-    html_code = """
+def generate_html(df, table_name):
+    html_code = f"""
     <font size="4" face="Helvetica Neue">
     <head>
-        <title>Generated Table</title>
+        <title>{table_name}</title>
         <style>
-            caption {
+            caption {{
                 padding: 20px;
-            }
-            table, th, td {
+            }}
+            table, th, td {{
                 padding: 15px;
                 border: 1px solid black; 
                 border-collapse: collapse;
                 font-family: "Helvetica Neue";
                 font-weight: "45";
-            }
+            }}
         </style>
     </head>
     <body>
     <table width="100%">
-        <caption>Generated Table</caption>
+        <caption>{table_name}</caption>
         <tr>
     """
     # Adding table headers
@@ -78,6 +81,6 @@ elif manual_input:
 if df is not None:
     st.write("Data Preview:")
     st.write(df)
-    html_code = generate_html(df)
+    html_code = generate_html(df, table_name)
     st.write("Generated HTML Code:")
     st.code(html_code, language='html')
